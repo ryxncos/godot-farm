966f5bf080097ea73f2e368ac5dc82125c9c2704
ryan <ryanclayton999@gmail.com>
2026-04-27 21:54:08 -0300

    fix: correção do click

--- scenes/farm/farm.gd
+++ scenes/farm/farm.gd
@@ -1,4 +1,6 @@
 extends Node2D
+@onready var soil: TileMapLayer = $Map/Soil
+@onready var player: Player = $Player
 
 
 func _ready() -> void:
@@ -6,4 +8,10 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	
+	if Input.is_action_just_pressed("click"):
+		var mouse = get_global_mouse_position() #Posição do mouse
+		var tile = soil.local_to_map(mouse)
+		soil.set_cell(tile, 0, Vector2i(3,14))
+		print(mouse) 
+	
